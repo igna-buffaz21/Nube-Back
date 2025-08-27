5a6a419f28f51aa2b22cd85ff144fc2fa0021b41
Ignacio <ignaciobuffaz73@gmail.com>
2025-08-27 09:20:06 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+const upload = multer({ dest: '/srv/proyecto-nube/uploads' });
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+})
+
+app.post('/upload', upload.single('file'), (req, res) => {
+
+    console.log(req.file); // Información del archivo subido
+
+    if (!req.file) {
+      return res.status(400).send('No se subió ningún archivo.');
+    }
+    
+    res.send(`Archivo ${req.file.originalname} subido correctamente!`);
+  });
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
