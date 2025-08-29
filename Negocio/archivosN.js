ac1cc1c6ccdb015963974664ef97d0c40ce3688b
Ignacio <ignaciobuffaz73@gmail.com>
2025-08-29 11:52:53 -0300

    nueva funcion agregar archivos v11

--- Negocio/archivosN.js
+++ Negocio/archivosN.js
@@ -15,7 +15,7 @@ class ArchivosNegocio {
 
         const carpetaExistente = await CarpetasDAO.obtenerCarpetaporId(folder_id);
 
-        if (carpetaExistente.length === 0) {
+        if (!carpetaExistente) {
             throw new Error('La carpeta no existe');
         }
 
